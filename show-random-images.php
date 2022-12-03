ecd82241dca56e2ca074feff271f94b09c10c7cd
Abhijitsarker <abhijitsarker03@gmail.com>
2022-12-03 15:34:34 +0600

    slider

--- show-random-images.php
+++ show-random-images.php
@@ -28,11 +28,13 @@ function load_plugin_scripts($hook_suffix)
 {
 
 
-    //     wp_enqueue_style('random493_bootstrap', PLUGINS_PATH_ASSETS . 'css/bootstrap.min.css');
     wp_enqueue_style('flickity_style', PLUGINS_PATH_ASSETS . 'css/flickity.css');
-    //     wp_enqueue_script('random493_bootstrap', PLUGINS_PATH_ASSETS . 'js/bootstrap.min.js', array(), false, true);
+    wp_enqueue_style('slider_bootstrap', PLUGINS_PATH_ASSETS . 'css/bootstrap.min.css');
+    wp_enqueue_style('slider_style', PLUGINS_PATH_ASSETS . 'css/slider.css');
+    // wp_enqueue_script('random493_bootstrap', PLUGINS_PATH_ASSETS . 'js/bootstrap.min.js', array(), false, true);
     wp_enqueue_script('flickity_script', PLUGINS_PATH_ASSETS . 'js/flickity.pkgd.js', array('jquery'), false, true);
     wp_enqueue_script('random493_script_jquery', PLUGINS_PATH_ASSETS . 'js/script.js', array('jquery'), false, true);
+    wp_enqueue_script('slider_script_jquery', PLUGINS_PATH_ASSETS . 'js/slider_script.js', array('jquery'), false, true);
 
     wp_localize_script(
         'random493_script_jquery',
@@ -67,6 +69,7 @@ function show_form_data()
 
 add_shortcode('form_data_values', 'show_form_data');
 add_shortcode('form_person_info', 'form_person_info_func');
+add_shortcode('show_slider', 'show_slider_func');
 
 function form_person_info_func()
 {
@@ -82,6 +85,115 @@ function form_person_info_func()
         <div class="carousel-cell">Slide 3</div>
 
 
+    </div>
+
+<?php
+}
+
+function show_slider_func()
+{
+    echo $_SESSION['form_field_person'];
+?>
+    <div class="slider ml-lg-5 ml-md-5 mt-5 py-lg-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="imgContainer ml-lg-n5 mt-lg-0 mt-md-0 ml-md-n5 mt-n5">
+                    <div class="image">
+                        <img src="1.png" />
+                    </div>
+                    <div class="image">
+                        <img src="2.png" />
+                    </div>
+                    <div class="image">
+                        <img src="3.png" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <!-- slider -->
+                <div id="product<a href=" https://www.jqueryscript.net/slider/">
+                    <a class="carousel slide carousel-fade pr-lg-5 py-lg-0 py-4" data-ride="carousel">Slider</a>"
+
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="content">
+                                <div class="<a href=" https://www.jqueryscript.net/time-clock/">date</a>">
+                                    26 December 2019
+                                </div>
+                                <div class="title">
+                                    Lorem ipsum dolor - one
+                                </div>
+                                <div class="desc">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit. Error itaque, libero dignissimos nihil aliquam
+                                    eveniet tenetur cupiditate consectetur quod modi
+                                    repellendus veniam, repellat iusto fugiat temporibus
+                                    officia facere nulla nam.
+                                </div>
+                                <div class="d-flex justify-content-center justify-content-lg-start">
+                                    <button class="btn readMoreBtn">
+                                        Read More
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="content">
+                                <div class="date">
+                                    26 December 2019
+                                </div>
+                                <div class="title">
+                                    Lorem ipsum dolor - two
+                                </div>
+                                <div class="desc">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit. Error itaque, libero dignissimos nihil aliquam
+                                    eveniet tenetur cupiditate consectetur quod modi
+                                    repellendus veniam, repellat iusto fugiat temporibus
+                                    officia facere nulla nam.
+                                </div>
+                                <div class="d-flex justify-content-center justify-content-lg-start">
+                                    <button class="btn readMoreBtn">
+                                        Read More
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="content">
+                                <div class="date">
+                                    26 December 2019
+                                </div>
+                                <div class="title">
+                                    Lorem ipsum dolor -three
+                                </div>
+                                <div class="desc">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit. Error itaque, libero dignissimos nihil aliquam
+                                    eveniet tenetur cupiditate consectetur quod modi
+                                    repellendus veniam, repellat iusto fugiat temporibus
+                                    officia facere nulla nam.
+                                </div>
+                                <div class="d-flex justify-content-center justify-content-lg-start">
+                                    <button class="btn readMoreBtn">
+                                        Read More
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- indicators -->
+                    <ol class="carousel-indicators indicators">
+                        <li data-target="#productSlider" data-slide-to="0" class="active"></li>
+                        <li data-target="#productSlider" data-slide-to="1"></li>
+                        <li data-target="#productSlider" data-slide-to="2"></li>
+                    </ol>
+                    <!-- indicators -->
+                </div>
+                <!-- slider -->
+            </div>
+        </div>
     </div>
 
 <?php
